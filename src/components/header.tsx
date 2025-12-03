c6197a72a64294b36dea19742dceee53ee93bdd8
saewiam <saewiam@gmail.com>
2025-12-02 23:53:31 -0800

    Deleted template code, removed default margins, added header with first/last name

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import { css } from "@emotion/css";
+
+export default function Header() {
+  return (
+    <header className={css`
+        
+        `}>
+        <h1>Marisa Cantor</h1>
+    </header>
+  );
+}

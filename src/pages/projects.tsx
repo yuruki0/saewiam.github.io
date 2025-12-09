5749979f0b57f98843fd1888a641d7743fb5f610
saewiam <saewiam@gmail.com>
2025-12-08 21:33:14 -0800

    Worked on image loading

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,20 +4,64 @@ import { rootRoute } from './__root'
 import classes from './projects.module.css'
 import Image from '../components/art-display/image'
 
-import makeda from '../../assets/images/makeda sketch1.png'
-import vien from '../../assets/images/vien sketch1.png'
+import imageIndex from '../../assets/images/image-index.json'
+
+import { useEffect, useState } from "react";
+
+interface ImageDataType {
+    name: string,
+    caption: string,
+    date: string,
+    fileName: string
+}
 
 function ProjectsPage() {
-  return (
-    <>
-        <h2>prawjects</h2>
-        <div className={classes.projects_display}>
-            <Image src={makeda} caption ='sketches'/>
-            <Image src={vien} caption='sketches 2'/>
-        </div>
-    </>
-  )
+  const [art, setArt] = useState([...imageIndex.projects])
+
+    useEffect(() => {
+        (async () => {
+            const images: ImageDataType[] = [...imageIndex.projects]
+            for (let i = 0; i < imageIndex.projects.length; i ++) {
+                const imageData = imageIndex.projects[i]
+                const imgUrl = await import(`../../assets/images/${imageData.fileName}.jpg`)
+                const newImageData: ImageDataType = {...imageData};
+                newImageData.fileName = imgUrl.default;
+                images[i] = newImageData
+            }
+            setArt(images)
+        })()
+    }, [])
+
+ return (    
+        <>
+            <h2>prawjects</h2>
+            <div className={classes.art_display}>
+                {
+                    art.map((image) =>
+                        <Image key={image.fileName} src={image.fileName} caption={image.caption} />
+                    )
+                }
+            </div>
+        </>
+    )
 }
+
+
+{/* // async function ProjectsPage() {
+//   const imageComponents = [];
+//   for (let image of imageIndex.projects){
+//     const imgUrl = await import(`../../assets/images/${image.fileName}.png`)
+//     imageComponents.push(<Image src={imgUrl} caption={image.caption}/>)
+//   }
+//   return (
+//     <>
+//         <h2>prawjects</h2>
+//         <div className={classes.projects_display}>
+//           {imageComponents}
+//         </div>
+//     </>
+//   )
+// } */}
 
 export const ProjectsRoute = createRoute({
   getParentRoute: () => rootRoute,
